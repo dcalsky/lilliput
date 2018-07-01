ac2de1c853e88276e303638c99dd4a8222e78f0b
dcalsky <dcalsky@163.com>
2018-07-01 20:56:57 +0800

    chore: disable maximum button

--- src/app.js
+++ src/app.js
@@ -4,23 +4,15 @@ require('./core/task');
 require('./core/project');
 require('./core/dataset');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow = null;
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', () => {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1080,
     height: 768,
@@ -28,11 +20,9 @@ app.on('ready', () => {
     minHeight: 768,
     acceptFirstMouse: true,
     titleBarStyle: 'hidden',
-    frame: false,
+    maximizable: false
   });
-  // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-  // Emitted when the window is closed.
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
